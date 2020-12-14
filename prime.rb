097b8782519c95277738fa10c95902efbd548fe6
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-14 22:32:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,18 @@
 # Add  code here!
 def prime?(int, k = 7)
+  if int <= 1 || int == 4
+    return false
+  end
+
+  if int <= 3
+    return true
+  end
+
+  k.times do
+    a = 2 + rand() % (int - 4)
+
+    if power(a, int - 1, int)
+      return false
+    end
+  end
+end
