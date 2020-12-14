973d843bd1a6e46b069e3f58a4b2f807d4265d73
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-14 22:38:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,25 +1,10 @@
 # Add  code here!
-# def get_coprime(n):
-#     while True:
-#         coprime = random.randrange(n)
-#         if math.gcd(coprime, n) == 1:
-#             return coprime
-#
-#
-# def fermat_primality(n, count = 1):
-#     for _ in range(count):
-#         a = get_coprime(n)
-#         if (a ** (n-1)) % n != 1:
-#             return False
-#     return True
-
-def prime?(n, count = 1)
-  count.times do
-    a = get_coprime(n)
-    if (a ** (n - 1)) % n != 1
-      return false
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
     end
   end
-
-  true
 end
