4867a8137c57883fc45fb124cd5e5f1995fc6fdd
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-14 22:19:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,38 +1,12 @@
 # Add  code here!
-def prime?(n, k = 30)
-  return true if n == 2
-
-  d = n - 1
-  r = 0
-  while d & 1 == 0 do
-    d = d / 2
-    r += 1
-  end
-
-  k.times do
-    a = 2 + rand(n - 4)
-    x = mod_exp(a, d, n)
-    next if x == 1 || x == (n - 1)
-
-    (r - 1).times do
-      x = mod_exp(x, 2, n)
-      return false if x == 1
-      break if x == (n - 1)
+def prime?(int, count = 7)
+  if count == 0
+    return true
+  else
+    if fermatTest(int)
+      return prime?(int, count -= 1)
     end
-    return false if x != (n - 1)
   end
 
-  true
-end
-
-def mod_exp(base, exp, n)
-  product = 1
-  base = base % n
-  while exp != 0 do
-    product = (product * base) % n if exp & 1 == 1
-    exp = exp >> 1
-    base = (base * base) % n
-  end
-
-  product
+  return false
 end
